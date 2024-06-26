be34742c60d2207a1387aa9e5d14a64c3bf79314
IbnuParvez <parvezfatma98@gmail.com>
2024-06-26 12:53:09 -0700

    adkajsdf

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
- * read_textfile - rds text file  print to standard output
- * @filename: name to be read
- * @letters: num of letter to read ,print
- * Return: the num of letters printed, or  it fails 0
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	int i, y;
+	ssize_t nrd, nwr;
 	char *buf;
 
 	if (!filename)
@@ -16,25 +18,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
+
+	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
-	{
-		free(buf);
-		return (0);
-	}
-	buf[i] = '\0';
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
 	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
-	{
-		free(buf);
-		return (0);
-	}
+
 	free(buf);
-	return (y);
+
+	return (nwr);
 }

5d6bad144ae1a4105ef86a059e510ab4f327304b
IbnuParvez <parvezfatma98@gmail.com>
2025-07-31 15:59:14 +0300

    third file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase and uppercase alphabets from a - z using putchar
+ *
+ * Return:  Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'B'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}

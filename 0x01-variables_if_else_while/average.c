021144bae4c2a6d68f9e4f90dd9958526bb4edc4
IbnuParvez <parvezfatma98@gmail.com>
2026-01-28 18:48:53 +0300

    Add average.c and remove math.h from circle.c

---
+++ 0x01-variables_if_else_while/average.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    float a, b, c, sum, average;
+    printf("Enter all the three numbers of your liking: \n");
+    scanf("%f %f %f", &a, &b, &c);
+    return 0;
+}

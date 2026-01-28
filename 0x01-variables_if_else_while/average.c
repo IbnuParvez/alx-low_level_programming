3db65797553bfb23102a920e0a02ecefd69e578e
IbnuParvez <parvezfatma98@gmail.com>
2026-01-28 19:20:26 +0300

    Print sum before computing average

--- 0x01-variables_if_else_while/average.c
+++ 0x01-variables_if_else_while/average.c
@@ -5,6 +5,7 @@ int main(){
     printf("Enter all the three numbers of your liking: \n");
     scanf("%f %f %f", &a, &b, &c);
     sum = a + b + c;
+    printf("The sum of the three inputs is: %f\n",sum);
     average = sum / 3;
     printf("Your average is %f\n",average);
     return 0;

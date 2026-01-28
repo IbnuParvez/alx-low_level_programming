3acf75287e668d0e27c2956ff15c1ea9e1c2f5da
IbnuParvez <parvezfatma98@gmail.com>
2026-01-28 18:40:02 +0300

    Add circle area calculator program

---
+++ 0x01-variables_if_else_while/circle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float radius, area;
+    const float π = 3.142;
+    printf("Kindly provide the radius you would like\n");
+    scanf("%f", &radius);
+    area = π * radius * radius;
+    printf("Calculating the area ...\n");
+    printf("The area is %f\n", area);
+    return 0;
+}

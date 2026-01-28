4b55103c295213f1e438b1e70be98f0e44d804c5
IbnuParvez <parvezfatma98@gmail.com>
2026-01-28 19:14:29 +0300

    Compute and print average of three numbers

--- 0x01-variables_if_else_while/average.c
+++ 0x01-variables_if_else_while/average.c
@@ -4,5 +4,8 @@ int main(){
     float a, b, c, sum, average;
     printf("Enter all the three numbers of your liking: \n");
     scanf("%f %f %f", &a, &b, &c);
+    sum = a + b + c;
+    average = sum / 3;
+    printf("Your average is %f\n",average);
     return 0;
 }

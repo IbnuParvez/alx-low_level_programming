9f17faa2cb66851b8245aabe46adc01dd4681b5c
IbnuParvez <parvezfatma98@gmail.com>
2026-03-17 15:12:34 +0300

    Add _abs implementation and test program

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+int _abs(int n) {
+    if (n < 0) {
+        return(-n);
+    } else {
+        return(n);
+    }
+}
+
+int main(void)
+{
+    int r;
+
+    r = _abs(-1);
+    printf("%d\n", r);
+    r = _abs(0);
+    printf("%d\n", r);
+    r = _abs(1);
+    printf("%d\n", r);
+    r = _abs(-98);
+    printf("%d\n", r);
+    return (0);
+}
